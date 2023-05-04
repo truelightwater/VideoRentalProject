4a6a9fcdb13bf71db68cca42374c66bb7e5156d0
truelightwater <truelightwater@gmail.com>
2023-05-04 17:03:46 +0900

    팩토리 클래스에 volatile / synchronized 적용

--- src/main/java/VideoProject/video/factory/ServiceFactory.java
+++ src/main/java/VideoProject/video/factory/ServiceFactory.java
@@ -14,15 +14,23 @@ public class ServiceFactory implements Factory {
     }
 
     // 2. private static 인스턴스 변수
-    private static ServiceFactory serviceFactory;
+    // volatile 사용하면 해당 변수를 사용하는 스레드는 항상 메인 메모리에서 값을 읽어오게 되어 최신 값을 읽을 수 있다.
+    private static volatile ServiceFactory serviceFactory;
 
-    // 프로그램이 시작되면 바로 메모리에 올라갈 수 있도록 하는 것은 아래처럼
+    // 프로그램이 시작되면 바로 메모리에 올라갈 수 있도록 하는 것은 아래처럼 가능하게 한다.
     // private static ServiceFactory serviceFactory = new ServiceFactory();
 
     // 3. public static getInstance() method 구현
     public static ServiceFactory getInstance() {
+
+        // 1) 다른 스레드가 이미 객체를 생성한 경우에 대비하여 락을 걸어 동기화를 수행하기 위함
         if (serviceFactory == null) {
-            serviceFactory = new ServiceFactory();
+            synchronized (ServiceFactory.class) {       // synchronized block
+                // 2) 락을 걸었기 때문에 락을 걸지 않고 객체 생성 코드를 실행할 수 있음
+                if (serviceFactory == null) {
+                    serviceFactory = new ServiceFactory();
+                }
+            }
         }
         return serviceFactory;
     }
